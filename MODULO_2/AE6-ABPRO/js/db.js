4c3e45b64356c596adba1b01828c54166a6f2338
sebastian salas <seba07977@gmail.com>
2024-04-09 15:45:58 -0400

    ae6 con datatable

---
+++ MODULO_2/AE6-ABPRO/js/db.js
@@ -0,0 +1,103 @@
+const productos = [
+    {
+        "nombreProducto": "Arroz blanco",
+        "marca": "Tucapel",
+        "categoria": "abarrotes",
+        "precio": 1990,
+        "stock": 100
+    },
+    {
+        "nombreProducto": "Fideos tallarines",
+        "marca": "Lucchetti",
+        "categoria": "abarrotes",
+        "precio": 890,
+        "stock": 50
+    },
+    {
+        "nombreProducto": "Lentejas",
+        "marca": "Tres Montes",
+        "categoria": "abarrotes",
+        "precio": 1290,
+        "stock": 25
+    },
+    {
+        "nombreProducto": "Carne molida",
+        "marca": "San Jorge",
+        "categoria": "carniceria",
+        "precio": 2990,
+        "stock": 80
+    },
+    {
+        "nombreProducto": "Pechuga de pollo",
+        "marca": "Agrosuper",
+        "categoria": "carniceria",
+        "precio": 4990,
+        "stock": 120
+    },
+    {
+        "nombreProducto": "Tomate",
+        "marca": "Propal",
+        "categoria": "verduleria",
+        "precio": 890,
+        "stock": 150
+    },
+    {
+        "nombreProducto": "Lechuga",
+        "marca": "Propal",
+        "categoria": "verduleria",
+        "precio": 590,
+        "stock": 100
+    },
+    {
+        "nombreProducto": "Leche descremada",
+        "marca": "Soprole",
+        "categoria": "lacteos",
+        "precio": 1490,
+        "stock": 75
+    },
+    {
+        "nombreProducto": "Queso cheddar",
+        "marca": "Colun",
+        "categoria": "lacteos",
+        "precio": 2990,
+        "stock": 60
+    },
+    {
+        "nombreProducto": "Vino tinto",
+        "marca": "Casillero del Diablo",
+        "categoria": "botilleria",
+        "precio": 4990,
+        "stock": 40
+    },
+    {
+        "nombreProducto": "Cerveza",
+        "marca": "Escudo",
+        "categoria": "botilleria",
+        "precio": 1290,
+        "stock": 150
+    },
+    {
+        "nombreProducto": "Chocolate",
+        "marca": "Nestlé",
+        "categoria": "confiteria",
+        "precio": 1990,
+        "stock": 100
+    },
+    {
+        "nombreProducto": "Galletas",
+        "marca": "Tritón",
+        "categoria": "confiteria",
+        "precio": 890,
+        "stock": 80
+    },
+    {
+        "nombreProducto": "Papas fritas",
+        "marca": "Lay's",
+        "categoria": "confiteria",
+        "precio": 1490,
+        "stock": 50
+    },
+];
+
+
+export default productos;
